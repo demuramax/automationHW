8959ae2cef34fca97df46226a04bd4655f9cee82
svarga <demuramax@gmail.com>
2024-04-18 22:43:51 +0300

    HW-2 1.1.0

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 #HW-2:
 cat_lst = ['pushok', 'murka', 'maya']
+#1st variant:
 print(f'{cat_lst[0]}, {cat_lst[1]}, {cat_lst[2]}')
+#2nd variant:
+print(*cat_lst, sep=', ')
+#3nd variant:
+print(', '.join(cat_lst))
 
