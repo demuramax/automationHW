01ee5d9733ed77cdb520bbc3396c96cd04efbd44
svarga <demuramax@gmail.com>
2024-05-29 23:15:34 +0300

    hw-19

---
+++ hw-19.py
@@ -0,0 +1,17 @@
+# There is a list consisting of dictionaries. Each dictionary describes a user and
+# has two keys: 'name' (str) and 'age' (int).
+# Create and output a list of users' names whose age is 18 years (including) or older.
+
+users = [
+    {'name': 'Luarvik L. Luarvik',
+     'age': 17},
+    {'name': 'Olaf Andvarafors',
+     'age': 18},
+    {'name': 'Brun Du Barnstokr',
+     'age': 19}
+]
+
+Result: ['Olaf Andvarafors', 'Brun Du Barnstokr']
+for x in users:
+    if x['age'] > 17:
+        print(x['name'])
